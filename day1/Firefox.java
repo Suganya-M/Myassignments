2609f73f3e5d278dc31f3f8dbd3ec5ee33720020
Suganya-M <159041429+Suganya-M@users.noreply.github.com>
2024-02-05 20:35:38 +0530

    Add files via upload
    day1

---
+++ day1/Firefox.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float browserVersion=100.2F;
+		String browserName="firefox";
+		boolean isVisible=true;
+		int releaseYear=1998;
+		char browserLogo='f';
+		System.out.println("Browser Version:"+browserVersion);
+		System.out.println("Browser Name:"+browserName);
+		System.out.println("Browser Visibility:"+isVisible);
+		System.out.println("Browser Release Year:"+releaseYear);
+		System.out.println("Browser Logo:"+browserLogo);
+	}
+
+}
---
+++ day1/Ifclass.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class Ifclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int age=20;
+     if(age>18) {
+    	 System.out.println("Person eligible for voting:"+"Age="+age);
+     }
+     else {
+    	 System.out.println("Person not eligible for voting:"+age);
+	}
+}}
